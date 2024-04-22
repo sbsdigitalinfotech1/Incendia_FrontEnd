b8cc3f084b44498ffe4ebd9d23626f1de04caed6
himanshu7988 <himanshu.lodhi.aligarh@gmail.com>
2024-04-22 10:44:11 +0530

    Loading page update

--- src/components/Loading/LoadingPage.js
+++ src/components/Loading/LoadingPage.js
@@ -1,14 +1,14 @@
-'use client'
+"use client";
 
-import React from 'react'
+import React from "react";
 import { Spinner } from "@nextui-org/react";
 
 function LoadingPage() {
   return (
-    <div className='w-screen h-full backdrop-blur'>
-<Spinner label="Secondary" color="secondary" labelColor="secondary"/>
+    <div className="w-screen h-screen backdrop-blur flex justify-center items-center">
+      <Spinner label="Please wait..." color="secondary" labelColor="secondary" />
     </div>
-  )
+  );
 }
 
-export default LoadingPage
+export default LoadingPage;
