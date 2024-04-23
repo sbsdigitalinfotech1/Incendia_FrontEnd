c53687dc4ffb59f0597ab99f0c0dbd7d31202abb
himanshu7988 <himanshu.lodhi.aligarh@gmail.com>
2024-04-23 18:01:17 +0530

    review section adde

--- src/components/ProductRowFilter/ProductRowFilter.js
+++ src/components/ProductRowFilter/ProductRowFilter.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 function ProductRowFilter({ products }) {
   return (
     <>
-      <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+      <div className="mt-6 grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
         {products.map((product, i) => (
           <Link href={`/products/${product.id}`} key={i}>
             <div className="">
